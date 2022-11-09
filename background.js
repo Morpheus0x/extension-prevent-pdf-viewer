f5c292bfab3b96b3862401e49884d59ed29abbbe
Morpheus0x <morpheus0x@pm.me>
2022-11-09 16:26:32 +0100

    removed debugging code

--- background.js
+++ background.js
@@ -9,29 +9,11 @@ browser.tabs.onCreated.addListener((tab) => {
 	/* if (details.title.toLowerCase().endsWith('.pdf')) {
 		browser.tabs.remove(details.id)
 	} */
-	if (tab.url === "about:newtab") {
-		console.log('created empty tab with id: ', tab.id);
-		// browser.tabs.remove(tab.id);
-	}
 });
-
-browser.webNavigation.onCreatedNavigationTarget.addListener((evt) => { 
-	console.log('onCreatedNavigationTarget: ', evt); 
-});
-
-browser.webNavigation.onBeforeNavigate.addListener((evt) => { 
-	console.log('onBeforeNavigate: ', evt); 
-});
-
-browser.webNavigation.onCommitted.addListener((evt) => { 
-	console.log('onCommitted: ', evt); 
-});
-
 
 browser.downloads.onCreated.addListener( (dl) => {
 	console.log('download created: ', dl)
 	if (dl.filename.toLowerCase().endsWith('.pdf')) {
-		// console.log('downloaded pdf: ', dl.url, 'filename: ', dl.url.split('/').slice(-1)[0])
 		if (browser.runtime.PlatformOs === 'win') {
 			preventPreview[dl.filename.split('\\').slice(-1)[0]] = true
 		} else {
