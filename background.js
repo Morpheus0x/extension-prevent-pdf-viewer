665818a2c14c187abb137a4485622bf85e0d20eb
Morpheus0x <morpheus0x@pm.me>
2022-11-13 18:23:38 +0100

    removed imports which apparently don't work for webextensions by copying the functions

--- background.js
+++ background.js
@@ -1,5 +1,30 @@
-import { getSetting } from './store.js'
-import { getFileName } from './helpers.js'
+async function getSetting(key) {
+  const userSettings = await browser.storage.sync.get(key).catch(storageError)
+  const policySettings = await browser.storage.managed.get(key).catch(() => undefined)
+  return userSettings[key] || policySettings[key] || defaultSettings[key];
+}
+// Modified from Source: https://stackoverflow.com/a/67994693
+function getFileName(disposition) {
+  const utf8FilenameRegex = /filename\*=(["']?)utf-8''([\w%\-\.]+)(?:; ?|$|\1)/i;
+  const asciiFilenameRegex = /^filename=(["']?)(.*?[^\\])\1(?:; ?|$)/i;
+
+  let fileName = null;
+  if (utf8FilenameRegex.test(disposition)) {
+    fileName = decodeURIComponent(utf8FilenameRegex.exec(disposition)[2]);
+  } else {
+    // prevent ReDos attacks by anchoring the ascii regex to string start and
+    //  slicing off everything before 'filename='
+    const filenameStart = disposition.toLowerCase().indexOf('filename=');
+    if (filenameStart >= 0) {
+      const partialDisposition = disposition.slice(filenameStart);
+      const matches = asciiFilenameRegex.exec(partialDisposition );
+      if (matches != null && matches[2]) {
+        fileName = matches[2];
+      }
+    }
+  }
+  return fileName;
+}
 
 let preventPreview = {};
 let preventDownload = {};
--- settings.js
+++ settings.js
@@ -1,4 +1,21 @@
-import { getSetting, setSetting, resetSettings } from './store.js'
+const defaultSettings = { saveAs: false }
+function storageError(e) {
+    console.error('storage error: ', e)
+    return undefined
+}
+async function getSetting(key) {
+    const userSettings = await browser.storage.sync.get(key).catch(storageError)
+    const policySettings = await browser.storage.managed.get(key).catch(() => undefined)
+    return userSettings[key] || policySettings[key] || defaultSettings[key];
+}
+async function setSetting(key, newValue) {
+    console.log('newValue: ', newValue, 'for key: ', key)
+    await browser.storage.sync.set({ [key]: newValue }).catch(storageError)
+}
+async function resetSettings() {
+    console.log('removed')
+    await browser.storage.sync.remove('saveAs').catch(storageError)
+}
 
 const resetButtonID = '#reset'
 const saveAsID = '#saveAs'
