f9b3c1d18f42709e2054849414ee4ec3b989bf0d
Morpheus0x <morpheus0x@pm.me>
2022-11-13 14:37:06 +0100

    moved helper function to own js lib file

--- background.js
+++ background.js
@@ -1,30 +1,8 @@
+import { getFileName } from './helpers.js'
+
 let preventPreview = {};
 let preventDownload = {};
 let preventDoubleDownload = {};
-
-// helper functions
-// Modified from Source: https://stackoverflow.com/a/67994693
-function getFileName(disposition) {
-  const utf8FilenameRegex = /filename\*=(["']?)utf-8''([\w%\-\.]+)(?:; ?|$|\1)/i;
-  const asciiFilenameRegex = /^filename=(["']?)(.*?[^\\])\1(?:; ?|$)/i;
-
-  let fileName = null;
-  if (utf8FilenameRegex.test(disposition)) {
-    fileName = decodeURIComponent(utf8FilenameRegex.exec(disposition)[2]);
-  } else {
-    // prevent ReDos attacks by anchoring the ascii regex to string start and
-    //  slicing off everything before 'filename='
-    const filenameStart = disposition.toLowerCase().indexOf('filename=');
-    if (filenameStart >= 0) {
-      const partialDisposition = disposition.slice(filenameStart);
-      const matches = asciiFilenameRegex.exec(partialDisposition );
-      if (matches != null && matches[2]) {
-        fileName = matches[2];
-      }
-    }
-  }
-  return fileName;
-}
 
 browser.tabs.onCreated.addListener((tab) => { 
   // Close any PDF Viewer tab that opens for downloaded PDFs
